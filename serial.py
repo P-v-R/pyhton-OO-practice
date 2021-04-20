cd0b396daffbc9b5f5198e81838e35eacde61f5b
Perryvon <Perry.j.vr@gmail.com>
2021-04-20 15:28:57 -0700

    first file complete

--- serial.py
+++ serial.py
@@ -1,5 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
+
     
     >>> serial = SerialGenerator(start=100)
 
@@ -17,3 +18,25 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    
+    def __init__(self, start):
+        self.start = start
+        self.counter = 0
+
+    def generate(self):
+        '''upon first generate(), display start number,
+           every time its called after it will return start + 1 '''   
+        current_num = self.start + self.counter 
+        self.counter += 1
+        return current_num
+    
+    def reset(self):
+        '''when called, resets the increment counter to
+           start back at beginning  '''
+        self.counter = 0
+
+    
+
+
+    
+
